db310a796af3f9a2d4766957fd92a63916e9604b
Akello-web <aklbktglv@gmail.com>
2024-07-17 20:11:05 +0500

    Covered with tests CustomerRequestServiceImpl methods

--- investment-advisory-service/src/main/java/kz/projects/ias/models/CustomerServiceRequest.java
+++ investment-advisory-service/src/main/java/kz/projects/ias/models/CustomerServiceRequest.java
@@ -3,12 +3,16 @@ package kz.projects.ias.models;
 import jakarta.persistence.*;
 import kz.projects.ias.models.enums.RequestStatus;
 import kz.projects.ias.models.enums.RequestType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "t_customer_requests")
 public class CustomerServiceRequest {
   @Id
