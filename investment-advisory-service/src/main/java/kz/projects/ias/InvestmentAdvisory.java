1f55cda7c7670aabd32d61d2bbf1253275f3051b
Akello-web <aklbktglv@gmail.com>
2024-07-17 23:58:48 +0500

    modified

--- investment-advisory-service/src/main/java/kz/projects/ias/InvestmentAdvisory.java
+++ investment-advisory-service/src/main/java/kz/projects/ias/InvestmentAdvisory.java
@@ -2,8 +2,9 @@ package kz.projects.ias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 public class InvestmentAdvisory {
   public static void main(String[] args) {
     SpringApplication.run(InvestmentAdvisory.class, args);
